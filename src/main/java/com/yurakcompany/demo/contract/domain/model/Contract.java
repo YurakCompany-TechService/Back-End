7d462b0dcc8adb81070db6f774194fc22a9b81fa
Sebastian Vivanco <89156377+wutdubs@users.noreply.github.com>
2022-06-12 05:20:02 -0500

    feat: added contract

---
+++ src/main/java/com/yurakcompany/demo/contract/domain/model/Contract.java
@@ -0,0 +1,29 @@
+package com.yurakcompany.demo.contract.domain.model;
+
+import com.yurakcompany.demo.register.domain.model.Business;
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "contracts")
+public class Contract {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @NotBlank
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = true)
+    @JoinColumn(name = "business_id", nullable = true)
+    private Business business;
+}
